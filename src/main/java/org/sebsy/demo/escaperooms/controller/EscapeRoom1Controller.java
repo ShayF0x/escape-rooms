62a8654d3344750876e548ae41124f7ea1954480
ShayFox <shayfox.escasia@gmail.com>
2025-11-14 17:43:40 +0100

    Exercice fait !

--- src/main/java/org/sebsy/demo/escaperooms/controller/EscapeRoom1Controller.java
+++ src/main/java/org/sebsy/demo/escaperooms/controller/EscapeRoom1Controller.java
@@ -1,5 +1,8 @@
 package org.sebsy.demo.escaperooms.controller;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class EscapeRoom1Controller {
 
     public void entreeSalle1() {
